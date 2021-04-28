6698104273ae5042176499e98a1c668d687be9a3
Luis Yerik Arambula <A00825080@itesm.mx>
2021-04-27 20:12:10 -0500

    agregar funcion de registro

--- botDesaparecidx/ViewControllerRegistrar.swift
+++ botDesaparecidx/ViewControllerRegistrar.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
 
 class ViewControllerRegistrar: UIViewController {
     @IBOutlet weak var tfNombre: UITextField!
@@ -15,6 +17,8 @@ class ViewControllerRegistrar: UIViewController {
     @IBOutlet weak var tfConfirmarContra: UITextField!
     @IBOutlet weak var btRegistrarse: UIButton!
     @IBOutlet weak var lbError: UILabel!
+    
+    var ref : DatabaseReference!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,5 +36,68 @@ class ViewControllerRegistrar: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // MARK: - Funciones de validacion de campos
+    
+    
+    
+    //Checar si la contrasena es valida
+    func isPasswordValid(_ password : String) -> Bool {
+        let passwordTest = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[a-z])(?=.*[$@$#!%*?&])[A-Za-z\\d$@$#!%*?&]{8,}")
+        return passwordTest.evaluate(with: password)
+    }
+    
+    //Checar si los campos son validos
+    func validateInputs() -> String? {
+        //Checar si los campos estan llenos
+        if tfNombre.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || tfApellido.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" || tfCorreo.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            tfContrasena.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            return "Por favor rellene todos los campos"
+        }
+        
+        let password = tfContrasena.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        //Checar si la contrasena es valida
+        if !(isPasswordValid(password)) {
+            return "Tu contraseña es inválida, asegúrate que tenga 8 caracteres, un caracter especial y un número"
+        }
+        
+        //Checar si las contrasenas coinciden
+        if tfContrasena.text != tfConfirmarContra.text{
+            return "Las contraseñas no coinciden"
+        }
+        return nil
+    }
 
+    //Funcion del boton de registro
+    
+    @IBAction func registroTap(_ sender: UIButton) {
+        let error = validateInputs()
+        
+        if error != nil{
+            lbError.text = error
+        }
+        else{
+            ref = Database.database().reference()
+            let correo = tfCorreo.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let nombres = tfNombre.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let apellidos = tfApellido.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let contrasena = tfContrasena.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            Auth.auth().createUser(withEmail: correo, password: contrasena) { (result, err) in
+              
+                //checar errores
+                if err != nil {
+                    self.lbError.text = "error creando usuario"
+                }
+                else{
+                    self.ref.child("USUARIOS_APP/\(result!.user.uid)").setValue(["nombre" : nombres,
+                        "apellidos" : apellidos,
+                        "contrasena" : contrasena,
+                        "correo" : correo
+                    ])
+                    self.lbError.text = "Usuario creado!"
+                }
+            }
+        }
+    }
 }
