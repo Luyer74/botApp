94b763831c7d0a868d25cdd95cc8e542025c520d
Juan Pablo <A00825316@itesm.mx>
2021-05-26 18:38:53 -0700

    Agregadas funciones a Buscar
    Faltaban funciones de Inicio para Buscar, sigue sin mostrar la lista

--- botDesaparecidx/ViewControllerBuscar.swift
+++ botDesaparecidx/ViewControllerBuscar.swift
@@ -42,6 +42,9 @@ class ViewControllerBuscar: UIViewController, UITableViewDelegate, UITableViewDa
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
 
         // Do any additional setup after loading the view.
         let tap = UITapGestureRecognizer(target: self, action: #selector(quitaTeclado))
@@ -137,5 +140,153 @@ class ViewControllerBuscar: UIViewController, UITableViewDelegate, UITableViewDa
         // Pass the selected object to the new view controller.
     }
     */
-
+    func loadData(){
+        isPaginating = true
+        var datosCasos = [[String : String]]()
+        getInitialData() { datos in
+            let group = DispatchGroup()
+            datosCasos = datos
+            if datosCasos.count == 0{
+                self.isPaginating = false
+                return
+            }
+            self.tableView.tableFooterView = self.createSpinnerFooter()
+            for i in 1...datos.count{
+                group.enter()
+                let id_usuario = datos[i-1]["id_usuario"]!
+                self.getUserName(userID: id_usuario, completion: { user_name , location in
+                    datosCasos[i-1]["nombre_usuario"] = user_name
+                    datosCasos[i-1]["lugar"] = location
+                    group.leave()
+                })
+            }
+            
+            for i in 1...datos.count{
+                group.enter()
+                let id_tweet = datos[i-1]["tweet_id"]!
+                self.getImageLink(tweetID: id_tweet, completion: {link in
+                    datosCasos[i-1]["link_imagen"] = link
+                    group.leave()
+                })
+            }
+            
+            group.notify(queue: DispatchQueue.global(), execute: {
+                self.insertData(datosCasos: datosCasos)
+                print("listo!")
+                self.reloadTable()
+                self.isPaginating = false
+            })
+        }
+    }
+    
+    
+    func getInitialData(completion: @escaping ([[String : String]]) -> Void){
+        var initData = [[String : String]]()
+        var cont = 0
+        let date_str = getDate()
+        print(date_str)
+        ref.child("TWEETS").queryOrdered(byChild: "date_created").queryStarting(atValue: date_str).queryEnding(atValue: date_str).observe(.value) { (snapshot) in
+            for snap in snapshot.children{
+                let data = snap as! DataSnapshot
+                let tweetID = data.key
+                if let valueDictionary = data.value as? [AnyHashable:AnyObject]{
+                    var userID = ""
+                    if let userIDini = valueDictionary["user"] as? NSNumber{
+                        userID = userIDini.stringValue
+                    }
+                    else if let userIDini = valueDictionary["user"] as? String{
+                        userID = userIDini
+                    }
+                    let fechacreado = valueDictionary["date_created"] as! String
+                    let tweet_text = valueDictionary["tweet_text"] as! String
+                    let dict = ["tweet_id" : tweetID, "fecha_creado" : fechacreado, "id_usuario" : userID, "tweet_text" : tweet_text]
+                    initData.insert(dict, at: 0)
+                }
+                cont = cont + 1
+            }
+            completion(initData)
+        }
+    }
+    
+    func getUserName(userID : String, completion: @escaping (String, String) -> Void){
+        self.ref.child("USERS/\(userID)").observeSingleEvent(of: .value, with: {(snapshot) in
+            var nombre_usuario = ""
+            var lugar = ""
+            if let valueDictionary = snapshot.value as? [AnyHashable:AnyObject]{
+                if let user_name = valueDictionary["name"] as? String {
+                    nombre_usuario = user_name
+                }
+                if let location = valueDictionary["location"] as? String{
+                    if location == ""{
+                        lugar = "Desconocido"
+                    }else{
+                        lugar = location
+                    }
+                }
+            }
+            completion(nombre_usuario, lugar)
+        })
+    }
+    
+    func getImageLink(tweetID: String, completion: @escaping (String?) -> Void){
+        self.ref.child("IMAGES/\(tweetID)/0/image_link").observeSingleEvent(of: .value, with: {(snapshot) in
+            if let value = snapshot.value as? String {
+                let image_link = value
+                completion(image_link)
+            }
+            else{
+                completion(nil)
+            }
+        })
+    }
+    
+    func reloadTable(){
+        DispatchQueue.main.async {
+            self.tableView.tableFooterView = nil
+            self.tableView.reloadData()
+        }
+    }
+    
+    func createSpinnerFooter() -> UIView{
+        let footerView = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 100))
+        let spinner = UIActivityIndicatorView()
+        spinner.center = footerView.center
+        footerView.addSubview(spinner)
+        spinner.startAnimating()
+        
+        return footerView
+    }
+    
+    func insertData(datosCasos : [[String : String]]){
+        for i in 1...datosCasos.count{
+            let tw = tweet(tweet_text: datosCasos[i-1]["tweet_text"]!, fecha_creado: datosCasos[i-1]["fecha_creado"]!, imagen_link: datosCasos[i-1]["link_imagen"], user_name: datosCasos[i-1]["nombre_usuario"]!, lugar: datosCasos[i-1]["lugar"]!)
+            self.listaCasos.append(tw)
+        }
+    }
+    
+    func getDate() -> String{
+        let nextDate = Calendar.current.date(byAdding: .day, value: daysPassed, to: fecha)
+        formater.dateFormat = "yyyy-MM-dd"
+        formater.timeZone = TimeZone(abbreviation: "UTC")
+        return formater.string(from: nextDate!)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        if let height = navigationController?.navigationBar.frame.height{
+            moveAndResizeImage(for: height)
+        }
+        guard !isPaginating else{
+            return
+        }
+        let position = scrollView.contentOffset.y
+        if position > (tableView.contentSize.height+180-scrollView.frame.size.height){
+            print("loading more...")
+            loadData()
+            daysPassed = daysPassed - 1
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
 }
